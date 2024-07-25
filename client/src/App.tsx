e3f3d0184d4656ae75824bd1631b2abda46ba555
ayush-that <ayush1337@hotmail.com>
2024-07-25 21:02:54 +0530

    chore: Add initial project files and configurations

---
+++ client/src/App.tsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import { Layout, Row, Col } from "antd";
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <Row align="middle">
+          <Col span={10} offset={2}>
+            <h1>Our todolist</h1>
+          </Col>
+          <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
+            <Col
+              span={12}
+              style={{ textAlign: "right", paddingRight: "200px" }}
+            >
+              <WalletSelector />
+            </Col>{" "}
+          </Col>
+        </Row>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
